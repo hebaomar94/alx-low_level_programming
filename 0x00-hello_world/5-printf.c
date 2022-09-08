a8e39ccc7f288ac3a552ef473452e5bce15a0ef7
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-08 14:39:48 -0700

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ *printf task
+ *Return: return 1 after program execution
+ */
+ int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	Return (0);
+}
