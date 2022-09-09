f202eabb9e4acad17a6cf16bc6d8b77a3d719a35
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-09 10:55:42 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/*
+ * main entry level
+ * *
+ * return 0 
+ */
+int main(void)
+{
+	int i ;
+	for ( i =0 ; i < 10 ; i++)
+	{
+		printf("%d",i);
+	}
+
+	print("\n");
+return 0
+}
