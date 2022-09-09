37048603fd62f9ea1848719b9adaf7da04b82b4b
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-09 10:34:14 -0700

    5 git commit -m 4-print_alphabt.c git push
    git add .
    git commit -m 5

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main Enry level
+ * *
+ * return 0
+ */
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet='a' ; alphabet <=z ; alphabet++)
+	{
+		if (alphabet == 'q')
+			continue;
+		if (alphabet == 'e')
+			continue;
+		putchar(alphabet);
+	}
+
+	putchar("\n")
+
+		return (0)
+
