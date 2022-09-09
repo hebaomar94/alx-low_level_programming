6fa241037aea3341a1b4c95d6ca166f0dcba4cc8
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-09 09:34:38 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *   *
+ * Return: Always 0 (Sucess/correct)
+ */
+
+int main()
+{
+
+	    for(int x = 'A'; x <= 'Z'; x++)
+{
+				        x = tolower(x);
+					putchar(x);
+}
+
+	        return 0;
+}
