23b74a791cbadc0c9abf94babcdebaf4d6ac926b
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-09 10:14:45 -0700

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,14 +7,15 @@
  */
 int main(void)
 {
-
 	char lower_case;
 	char upper_case;
 
 	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
 	{
 		putchar(lower_case);
-	}	
+	}
+
+
 	for (upper_case = 'A'; upper_case <= 'Z'; upper_case++)
 	{
 		putchar(upper_case);
