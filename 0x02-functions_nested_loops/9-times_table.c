42339b623354adcb37a61f9e0f0e01bfc53075d3
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-13 16:55:57 -0700

    9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -6,6 +6,7 @@
  * Return: Always 0.
  */
 
+
 void times_table(void)
 
 {
@@ -20,70 +21,69 @@ void times_table(void)
 
 	{
 
-		for (b = 0; b <= 9; b++)
+	for (b = 0; b <= 9; b++)
 
-		{
+	{
 
-			c = a * b;
+	c = a * b;
 
-			if ((c / 10) == 0)
+	if ((c / 10) == 0)
 
-			{
+	{
 
-				if (b == 0)
+	if (b == 0)
 
-				{
+	{
 
-					_putchar ('0');
+	_putchar ('0');
 
-				}
+	}
 
-				if (b != 0)
+	if (b != 0)
 
-				{
+	{
 
-					_putchar (' ');
+	_putchar (' ');
 
-					_putchar ((c % 10) + '0');
+	_putchar ((c % 10) + '0');
 
-				}
+	}
 
-				if (b < 9)
+	if (b < 9)
 
-				{
+	{
 
-					_putchar(',');
+	_putchar(',');
 
-					_putchar (' ');
+	_putchar (' ');
 
-				}
+	}
 
-			}
+	}
 
-			else
+	else
 
-			{
+	{
 
-				_putchar ((c / 10) + '0');
+	_putchar ((c / 10) + '0');
 
-				_putchar ((c % 10) + '0');
+	_putchar ((c % 10) + '0');
 
-				if (b < 9)
+	if (b < 9)
 
-				{
+	{
 
-					_putchar(',');
+	_putchar(',');
 
-					_putchar (' ');
+	_putchar (' ');
 
-				}
+	}
 
-			}
+	}
 
-		}
+	}
 
-		_putchar ('\n');
-
+	_putchar ('\n');
 	}
 
 }
