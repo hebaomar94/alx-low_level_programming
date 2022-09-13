f1ba7d50bcb3e06569eb6a8137425c47c31ac1d1
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-13 14:49:34 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - check the code.
+ *   *
+ * Return: Always 0.
+ */
+
+int main(void)
+{
+
+
+
+	if (c > 'a' && c < 'z')
+
+	{
+
+		return (1);
+
+	}
+
+
+
+	else
+
+	{
+
+		return (0);
+
+	}
+
+
+
+}
