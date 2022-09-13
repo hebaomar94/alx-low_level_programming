9e3bf93e7ac24af19ae84cc133f7eeb188c56380
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-13 13:17:47 -0700

    1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -2,20 +2,19 @@
 /**
  * print_alphabet - print alphabet lowercase
  * *
- * Return: Always 0.
+ * Return : always (0)
  */
-
-
-
 void print_alphabet(void)
-{
-	char x;
-	
-	for(x = 'a' ; x <= 'z' ; x++)
-	{
-		x=tolower (x);
-		putchar(x)
-	}
 
-	return (0) ;
+{
+
+		char i;
+
+
+
+			for (i = 'a'; i <= 'z'; i++)
+
+						_putchar(i);
+
+				_putchar('\n');
 }
