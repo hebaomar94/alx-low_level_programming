ffeefa78eb9e5124bceffb5579ec942aa30b6016
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-15 15:31:04 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * _isupper - evalue if letter is uppercase .
+ *@c: print int
+ * Return: Always 0.
+ */
+
+int _isupper(int c)
+{
+	for(i > "A" ; i < "Z" ; i++)
+	{
+		if(i == "c" )
+		
+		_putchar(1);
+
+	else 
+	{
+		_putchar(0);
+	}
+	
+	_putchar("\n");
+	}
