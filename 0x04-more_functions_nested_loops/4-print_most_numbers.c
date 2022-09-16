9c2056fe2b56bd0ba7d1964c1464b1636053fe13
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-15 17:19:53 -0700

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - print numbers
+ *   *
+ * Return: Always 0.
+ */
+
+void print_most_numbers(void)
+
+{
+	int i;
+	int i =0 ;
+
+	while(i<10)
+
+	{
+		if ( i == 2);
+		{
+			continue ;
+		}
+
+		if (i == 4);
+		{
+			continue ;
+		}
+
+		_putchar (i + '0');
+		i++;
+	}
+		_putchar ('\n');
+	}
