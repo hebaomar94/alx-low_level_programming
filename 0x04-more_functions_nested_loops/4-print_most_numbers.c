630a95135b48e30dbcee01ef2d2dbda9fd58d9dc
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-15 17:26:07 -0700

    4-print_most_numbers.c

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,35 +7,35 @@
  */
 
 void print_most_numbers(void)
+
 {
+
+
+
+	int i;
+
+
+
+	i = 0;
+
+	while (i < 10)
+
 	{
 
-
-
-		int i;
-
-
-
-		i = 0;
-
-		while (i < 10)
+		if (i != 2 && i != 4)
 
 		{
 
-			if (i != 2 && i != 4)
-
-			{
-
-				_putchar (i + '0');
-
-			}
-
-			i++;
+			_putchar (i + '0');
 
 		}
 
-
-
-		_putchar ('\n');
+		i++;
 
 	}
+
+
+
+	_putchar ('\n');
+
+}
