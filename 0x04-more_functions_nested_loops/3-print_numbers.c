c01c952a4e7830984591beb6a7bbc44a3d829141
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-15 17:04:00 -0700

    3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -6,20 +6,27 @@
  */
 
 void print_numbers(void)
-i = 0;
-
-while (i < 10)
 
 {
 
-	_putchar (i + '0');
-
-	i++;
-
-}
+	int i;
 
 
 
-_putchar ('\n');
+	i = 0;
+
+	while (i < 10)
+
+	{
+
+		_putchar (i + '0');
+
+		i++;
+
+	}
+
+
+
+	_putchar ('\n');
 
 }
