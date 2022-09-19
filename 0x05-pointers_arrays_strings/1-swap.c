f20dcf94dcc99294093fe7ef97af1abb2154dd06
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-19 14:08:41 -0700

    1-swap.c

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *swap_int - swaps the value of two intergers
+ *@a: first pointer
+ *@b: second pointer
+ */
+
+void swap_int(int *a, int *b)
+
+{
+
+		      int c = *a;
+
+
+
+			*a = *b;
+
+			*b = c;
+
+}
