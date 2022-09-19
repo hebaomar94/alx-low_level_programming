e9b0c987489c401f0b5b28a15c328b66eb04570b
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-19 15:26:49 -0700

    4-print_rev.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -8,17 +8,25 @@
  * Return: Nothing
  */
 
-
-
-void print_rev(char *s)
 {
-	int index;
+		int index;
 
-	for (index = 0; s[index] != '\0'; ++index);
 
-	for (--index; index >= 0; --index);
-	_putchar(s[index]);
 
-	_putchar('\n');
+			/*finds the length of string without null character*/
+
+			for (index = 0; s[index] != '\0'; ++index)
+
+						;
+
+
+
+				/*print char from the last index as you decrement*/
+
+				for (--index; index >= 0; --index)
+
+							_putchar(s[index]);
+
+					_putchar('\n');
 
 }
