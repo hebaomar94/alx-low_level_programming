aea004560441a74cab1add385eb269f07b5d05a3
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-19 15:14:14 -0700

    3-puts.c

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,30 +3,27 @@
 
 
 /**
- *
- *  * _puts - prints a string, followed by a newline to stdout
- *
- *   *
- *
- *    * @str: string parameter to print
- *
- *     *
- *
- *      * Return: Nothing
- *
- *      */
+ ** _puts - prints a string, followed by a newline to stdout
+ * @str: string parameter to print
+ * Return: Nothing
+ */
 
+
+
+void _puts(char *str)
 
 {
-	int i;
 
-	i = 0;
-	while (*str != '\0')
-	{
-		_putchar(*str + 0);
-		++str;
+		while (*str != '\0')
 
-	}
-	_putchar('\n');
+				{
+
+							_putchar(*str + 0);
+
+									++str;
+
+										}
+
+			_putchar('\n');
 
 }
