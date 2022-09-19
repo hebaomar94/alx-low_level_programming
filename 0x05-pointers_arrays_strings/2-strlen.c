caf3ca818bb6aec15d4442e5f484ede1d33ead12
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-19 14:27:39 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *_strlen - return the length of a str
+ *@s: string
+ *Return: lenght of a str
+ */
+
+int _strlen(char *s)
+{
+		int i;
+
+
+		i = 0 ;
+
+		while (*s != '\0')
+		{
+			i++;
+			s++;
+
+		}
+
+		return (i);
+
+}
