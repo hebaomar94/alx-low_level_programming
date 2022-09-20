5879d9564d7e5ddcf4898133f1d555405010c075
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-20 05:49:05 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -9,31 +9,24 @@
 void puts2(char *str)
 
 {
-
 		int i;
 
 		int j = 0;
 
 
-
 		while (str[j] != '\0')
-
-	{
+		{
 
 		j++;
-
-										        }
-
+		
+		}
 
 	 for (i = 0; i < j; i += 2)
-
-	{
+	 {
 
 										_putchar(str[i]);
-
-											}
-
-
+	 
+	 }
 
  _putchar('\n');
 
