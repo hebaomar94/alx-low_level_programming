4852588ae7640847479cbd3ad46e9fceac7c33c4
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-21 10:50:27 -0700

    1-strncat.c

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,4 +1,4 @@
-include "main.h"
+#include "main.h"
 
 /**
  *_strncat - concatenates two strings
@@ -7,24 +7,51 @@ include "main.h"
  *@n: value
  *Return: dest
  */
+
+
+
 char *_strncat(char *dest, char *src, int n)
+
+
+
 {
-	int i , j
-	
-	i=0;
 
-	while(dest[i] != '\0')
-	{
-		i++
-	}
+	int i, j;
 
-	j=0;
-	while(j < n && src [j] != '\0')
+
+
+	i = 0;
+
+	while (dest[i] != '\0')
+
 	{
-		dest[i] =src [j];
-		j++;
+
 		i++;
+
 	}
+
+
+
+	j = 0;
+
+	while (j < n && src[j] != '\0')
+
+	{
+
+		dest[i] = src[j];
+
+		j++;
+
+		i++;
+
+	}
+
+
+
 	dest[i] = '\0';
+
+
+
 	return (dest);
+
 }
