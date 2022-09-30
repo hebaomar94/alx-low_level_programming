89c41370a6b70688ec8c4d68bc7b23db29682ddb
hebaomar94 <hebaomar93.ho@gmail.com>
2022-09-30 13:50:33 -0700

    0-whatsmyname.c

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,29 +3,19 @@
 
 
 /**
- *
- *  * main - prints name of program followed by a new line
- *
- *   * @argc: number of arguments
- *
- *    * @argv: array containing arguments passed to the program
- *
- *     *
- *
- *      * Return: nothing
- *
- *       */
-
-
+ * main - prints name of program followed by a new line
+ * @argc: number of arguments
+ * @argv: array containing arguments passed to the program
+ * Return: nothing
+ */
 
 int main(int argc, char *argv[])
-
 {
 
 		(void)argc;
 
-			printf("%s\n", argv[0]);
+		printf("%s\n", argv[0]);
 
-				return (0);
+		return (0);
 
 }
