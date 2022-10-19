893cf84c3a5b3403fde475379bdccf0053bf0f9c
hebaomar94 <hebaomar93.ho@gmail.com>
2022-10-19 16:44:10 -0700

    2-calloc.c

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -1,11 +1,11 @@
 #include "main.h"
 
 /**
- *  *_calloc -allocated memoria for nmeb elemn de zise bytes
- *   *@nmemb: number of element in the array
- *    *@size: bytes for each position in array
- *     *Return: pointer void
- *      */
+ *_calloc -allocated memoria for nmeb elemn de zise bytes
+ *@nmemb: number of element in the array
+ *@size: bytes for each position in array
+ *Return: pointer void
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
 	char *p;
