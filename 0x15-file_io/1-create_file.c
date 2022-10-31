dd34894925e509ffe39b70e80a3d859a5873fde4
hebaomar94 <hebaomar93.ho@gmail.com>
2022-10-31 10:12:36 -0700

    1-create_file.c

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -9,7 +9,7 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int o, w, len= 0;
+	int o, w, len = 0;
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	w = write(o, text_content, len);
 
 	if (o == -1 || w == -1)
-		return(-1);
+		return (-1);
 	close(o);
 
 	return (1);
