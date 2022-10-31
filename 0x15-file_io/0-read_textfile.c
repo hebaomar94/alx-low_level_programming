4b912bdc66de07929e315f54728fe94ceabcdee4
hebaomar94 <hebaomar93.ho@gmail.com>
2022-10-31 09:58:48 -0700

    0-read_textfile.c

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -20,7 +20,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	o = open(filename, O_RDONLY);
 	r = read(o, buffer, letters);
-	w = write(STDOUT_FILEND, buffer, r);
+	w = write(STDOUT_FILENO, buffer, r);
 
 	if (o == -1 || r == -1 || w == -1 || w != r)
 	{
