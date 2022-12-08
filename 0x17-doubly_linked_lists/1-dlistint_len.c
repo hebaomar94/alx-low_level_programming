7863534f39ab8fb9b7483162f6fca5b43554bc3d
hebaomar94 <hebaomar93.ho@gmail.com>
2022-12-08 14:06:25 -0800

    1-dlistint_len.c

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ *
+ *  * dlistint_len - prints the number of elements
+ *
+ *   * @h: pointer to head
+ *
+ *    * Return: the number of elements
+ *
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+	dlistint_t *temp;
+
+	if (h == NULL)
+		return (0);
+	temp = (dlistint_t *)h;
+	while (temp != NULL)
+	{
+		temp = temp->next;
+		count += 1;
+	}
+	return (count);
+
+}
+
