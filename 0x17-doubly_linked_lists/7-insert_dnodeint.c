a84bcf3f158e93b0e1657f1868b00d2a017c83bb
hebaomar94 <hebaomar93.ho@gmail.com>
2022-12-08 15:06:34 -0800

    7-insert_dnodeint.c

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -1,50 +1,94 @@
 #include "lists.h"
 
+
+
 /**
- * insert_dnodeint_at_index - function to insert new node at given index
- * @h: input pointer to head of dlistint_t list
- * @idx: input index or n to add new node at, starting from 0
- * @n: input data to add for new node
- * Return: the node at index or NULL
- */
+ *
+ *  * insert_dnodeint_at_index - Inserts a new node in a dlistint_t
+ *
+ *   *                            list at a given position.
+ *
+ *    * @h: A pointer to the head of the dlistint_t list.
+ *
+ *     * @idx: The position to insert the new node.
+ *
+ *      * @n: The integer for the new node to contain.
+ *
+ *       *
+ *
+ *        * Return: If the function fails - NULL.
+ *
+ *         *         Otherwise - the address of the new node.
+ *
+ *          */
+
 dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+
 {
-	dlistint_t *new_node, *temp;
-	unsigned int c = 0;
 
-	new_node = malloc(sizeof(dlistint_t));
-	if (new_node == NULL)
-		return (NULL);
-	new_node->n = n;
-	new_node->prev = NULL;
-	new_node->next = NULL;
+		dlistint_t *tmp = *h, *new;
 
-	if (h == NULL)
-		return (new_node);
-	if (idx == 0)
-	{
-		new_node->next = (*h);
-		if ((*h) != NULL)
-			(*h)->prev = new_node;
-		*h = new_node;
-		return (new_node);
-	}
+		if (idx == 0)
+						
+			return (add_dnodeint(h, n));
 
-	temp = *h;
-	while (temp != NULL)
-	{
-		c++;
-		if (c == idx)
+
+
+				
+		for (; idx != 1; idx--)
+
+						
 		{
-			new_node->prev = temp;
-			new_node->next = temp->next;
-			if (temp->next != NULL)
-				temp->next->prev = new_node;
-			temp->next = new_node;
-			return (new_node);
+									
+			tmp = tmp->next;
+											
+			if (tmp == NULL)
+															
+				return (NULL);
+												
 		}
-		temp = temp->next;
-	}
-	free(new_node);
-	return (NULL);
+
+
+					
+		if (tmp->next == NULL)
+
+								
+			return (add_dnodeint_end(h, n));
+
+
+
+						
+		new = malloc(sizeof(dlistint_t));
+
+							
+		if (new == NULL)
+
+										
+			return (NULL);
+
+
+								
+		new->n = n;
+
+									
+		new->prev = tmp;
+
+										
+		new->next = tmp->next;
+											
+		tmp->next->prev = new;
+												
+		tmp->next = new;
+
+
+													
+		return (new);
 }
+
+Footer
+
+© 2022 GitHub, Inc.
+
+Footer navigation
+
+
